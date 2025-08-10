07cb87be2823719f008c1d9e1e60fce076123012
aajuvale <ahmed.juvale@gmail.com>
2025-08-09 17:09:29 -0700

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Ahmed Juvale on 8/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

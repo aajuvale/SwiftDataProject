1c4a30189192da6028b50ce32a43f0d5597210cb
aajuvale <ahmed.juvale@gmail.com>
2025-08-11 20:32:34 -0700

    feat: implemented relationships query

--- SwiftDataProject/User.swift
+++ SwiftDataProject/User.swift
@@ -14,6 +14,9 @@ class User {
     var city: String
     var joinDate: Date
 
+    @Relationship(deleteRule: .cascade)
+    var jobs = [Job]()
+
     init(name: String, city: String, joinDate: Date) {
         self.name = name
         self.city = city
